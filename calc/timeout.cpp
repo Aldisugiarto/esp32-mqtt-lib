d1590ad79ead65a30fc718c4a51920f667a3afab
Aldi Sugiarto <aldi.sugiarto@bluebirdgroup.com>
2024-09-06 10:29:51 +0700

    init module for project

---
+++ calc/timeout.cpp
@@ -0,0 +1,5 @@
+#include "timeout.h"
+
+int add_timeout(int val1, int val2){
+    return val1 + val2;
+}
---
+++ calc/timeout.h
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_TIMEOUT_H_
+#define INCLUDE_TIMEOUT_H_
+
+#include <stdio.h>
+
+int add_timeout(int val1, int val2);
+
+#endif
